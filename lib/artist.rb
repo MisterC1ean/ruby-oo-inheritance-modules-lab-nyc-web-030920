1a341f91a9989bb7ee3814a98694f14833e664ac
MisterC1ean <robert_keller44@icloud.com>
2020-03-16 10:51:22 -0400

    commit 1

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,6 +3,9 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
+  
+  extend Memorable::ClassMethods
+  
 
   @@artists = []
 
@@ -11,31 +14,38 @@ class Artist
     @songs = []
   end
 
+  #finds an Artist instance by matching name
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
   end
 
+  #returns an array of all artist instances
   def self.all
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # #clears the array of all artists so its empty
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # #returns number of artist instances
+  # def self.count
+  #   self.all.count
+  # end
 
+  #adds to the song array and makes artist self
   def add_song(song)
     @songs << song
     song.artist = self
   end
 
+  #adds multiple songs to songs array
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
 
+  #makes name lowercase and removes spaces and - ? ? 
   def to_param
     name.downcase.gsub(' ', '-')
   end
